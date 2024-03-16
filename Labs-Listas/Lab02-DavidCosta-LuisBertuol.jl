3ad95bf2bb0ebb7aba3f8dabe12b744ed193e453
Luis13777 <luis.bertuol.8865@ga.ita.br>
2024-03-16 14:32:42 -0300

    Fatoracao LU com e sem pivoteamento, falta usar isso para resolver o sistema linear

---
+++ Labs-Listas/Lab02-DavidCosta-LuisBertuol.jl
@@ -0,0 +1,251 @@
+# =====================================================================
+# *********************************************************************
+#                    MAT-55 2024 - Lista 01 - Exercício 6 
+# *********************************************************************
+# =====================================================================
+
+# =====================================================================
+#                    Algoritmo de Substituição Direta
+# =====================================================================
+# ---------------------------------------------------------------------
+# Dados de entrada:
+# A     matriz nxn triangular inferior, não singular
+# b     vetor n
+# ---------------------------------------------------------------------
+# Saída:
+# b     se A é não singular, b é a solução do sistema linear Ax = b
+
+function sub_direta(A::Array{Float64,2}, b::Array{Float64,1})
+
+    tol = 1e-12
+    n = length(b)
+    
+    # -----------------------------------------------------------------
+    #Teste se a matriz é triangular inferior
+    # Digite seu código aqui
+    for i in 1:n
+        if abs(A[i,i]) < tol
+            error("Matriz singular")
+        end
+        for j in (i + 1):n
+            if abs(A[i,j]) > tol
+                error("A matriz não é triangular inferior")
+            end
+        end
+    end
+
+    # -----------------------------------------------------------------
+
+    for i = 1:n
+        s = 0.0
+        for j = 1:i-1
+            s = s + A[i,j]*b[j] # OU s += A[i,j]*b[j]
+        end
+        b[i] = ( b[i] - s ) / A[i,i]
+    end
+    return b
+end
+
+# =====================================================================
+#                    Algoritmo de Substituição Inversa
+# =====================================================================
+# ---------------------------------------------------------------------
+# Dados de entrada:
+# A     matriz nxn triangular superior, não singular
+# b     vetor n
+# ---------------------------------------------------------------------
+# Saída:
+# b     se A é não singular, b é a solução do sistema linear Ax = b
+
+function sub_inversa(A::Array{Float64,2}, b::Array{Float64,1})
+    
+    #Digite seu código aqui
+    tol = 1e-12
+    n = length(b)
+    # teste se a matriz não é triangular superior ou singular
+    for i in 1:n
+        if abs(A[i,i]) < tol
+            error("Matriz singular")
+        end
+        for j in 1:i-1
+            if abs(A[i,j]) > tol
+                error("A matriz não é triangular superior")
+            end
+        end
+    end
+
+    for i in n:-1:1
+        s = 0.0
+        for j = i+1:n
+            s += A[i,j]*b[j] # OU s += A[i,j]*b[j]
+        end
+        b[i] = (b[i] - s) / A[i,i]
+    end
+
+    return b
+end
+
+# =====================================================================
+#                    Algoritmo de Eliminação Gaussiana
+# =====================================================================
+# ---------------------------------------------------------------------
+# Dados de entrada:
+# A     matriz nxn triangular superior, não singular
+# b     vetor n
+# ---------------------------------------------------------------------
+# Saída:
+# b     se A é não singular, b é a solução do sistema linear Ax = b
+
+function elim_gauss(A::Array{Float64,2}, b::Array{Float64,1})
+
+
+    #Digite seu código aqui
+    n = length(b)
+    for i in 1:n
+        # pivoteamento parcial
+        max_index = i
+        for j in (i+1):n
+            if abs(A[j,i]) > abs(A[max_index,i])
+                 max_index = j
+            end
+        end
+        A[i, :], A[max_index, :] = A[max_index, :], A[i, :]
+        b[i], b[max_index] = b[max_index], b[i]
+        for j in (i+1):n
+            fator = A[j,i] / A[i,i]
+            for k in i:n
+                A[j,k] -= fator * A[i,k]
+            end
+            b[j] -= fator * b[i]
+        end
+    end
+    # Substituição inversa
+    sub_inversa(A, b)
+end
+
+# =====================================================================
+# =====================================================================
+#			PROGRAMA PRINCIPAL
+# =====================================================================
+# =====================================================================
+
+# Implemente um programa para resolver o sistema linear Ax = b, com 
+#opção para o usuário fornecer a matriz A, o vetor b e escolher o método 
+#utilizado, dentre as opçõe:
+# a: Algoritmo de Substituição Direta.
+# b: Algoritmo de Substituição Inversa. 
+# C: Eliminação Gaussiana.
+function solverLinearSystem(A::Array{Float64,2}, b::Array{Float64,1})
+    println("Escolha o método para resolver o sistema linear:")
+    println("a: Algoritmo de Substituição Direta.")
+    println("b: Algoritmo de Substituição Inversa.")
+    println("c: Eliminação Gaussiana.")
+    metodo = readline()
+    metodo = metodo[1]
+    
+    if metodo == 'a'
+        return sub_direta(A, b)
+    elseif metodo == 'b'
+        return sub_inversa(A, b)
+    elseif metodo == 'c'
+        return elim_gauss(A, b)
+    else
+        return "Método inválido"
+    end
+end
+
+# Função para decomposição LU
+function decomposicaoLUSemPivoteamento(A)
+    n = size(A, 1)
+    L = zeros(n, n)
+    U = copy(A)
+
+    for i in 1:n
+        L[i, i] = 1
+    end
+
+
+    for i in 1:n-1
+        for j in i+1:n
+            m = U[j, i]/U[i, i]
+            L[j, i] = m
+            for k in i:n
+                U[j, k] = U[j, k] - m*U[i, k]
+            end
+        end
+    end
+
+    
+
+
+    println(L)
+    println(U)
+    return L, U
+end
+
+
+# Função para decomposição LU
+function decomposicaoLUComPivoteamento(A)
+    n = size(A, 1)
+    L = zeros(n, n)
+    U = copy(A)
+
+    for i in 1:n
+        L[i, i] = 1
+    end
+
+
+    for i in 1:n-1
+        # verificar qual linha tem o elemento U[i, i] de maior modulo
+        max = U[i,i]
+        linhaMax = i
+        for j in i:n
+            if U[j, i] > max
+                max = U[j, i]
+                linhaMax = j
+            end
+        end
+
+        # permutar linhas
+        U[i, :], U[linhaMax, :] = U[linhaMax, :], U[i, :]
+
+        for j in i+1:n
+            m = U[j, i]/U[i, i]
+            L[j, i] = m
+            for k in i:n
+                U[j, k] = U[j, k] - m*U[i, k]
+            end
+        end
+    end
+
+
+    println(L)
+    println(U)
+    return L, U
+end
+
+
+
+
+
+
+
+
+
+# Dados do sistema
+#Digite aqui os dados do sistema linear
+
+# Para funcionar corretamente, coloque pelo menos um dos números na forma de float
+
+b = [2.0; 5; 6]
+
+A = [
+    1.0 2 2;
+    2 -1 0;
+    4 3 1 
+]
+
+
+decomposicaoLUComPivoteamento(A)
+
+
